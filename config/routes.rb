3895b60238ee5b22e110250952908b1e6b626090
diegoaguevarar <diaguevararo@unal.edu.co>
2017-02-12 16:47:56 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-
   root to: 'pages#index'
+  
+  get 'aboutUS', to: 'pages#aboutUS'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
